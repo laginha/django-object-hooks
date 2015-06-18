82fb2dbd2ec743d4eb6631ef5d83ad37c51afc30
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-06-18 18:05:40 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(each.req) for each in install_reqs if each.req]
+
+
+setup(
+    name             = 'Django Object Hooks',
+    version          = '1.0.0',
+    author           = "Diogo Laginha",
+    author_email     = "diogo.laginha.machado@gmail.com",
+    url              = 'https://github.com/laginha/django-object-hooks',
+    description      = "Object-level Web Hook application",
+    packages         = find_packages(where='src'),
+    package_dir      = {'': 'src'},
+    install_requires = reqs,
+    extras_require   = {},
+    zip_safe         = False,
+    license          = 'MIT',
+    classifiers      = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+    ]
+)
